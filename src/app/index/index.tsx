38b125a82eac20f76e9be9e933105d6aa57686a7
Josiel Phelipe <josiephelipel265@gmail.com>
2025-10-24 15:31:28 -0400

    initial commit

---
+++ src/app/index/index.tsx
@@ -0,0 +1,20 @@
+import { Text, View } from "react-native"
+
+import { styles } from "./styles"
+
+export default function Index () {
+    return(
+        <View style={styles.container}>
+            <Text style={styles.titlea}>
+                Máquina de eventos
+            </Text>
+            <Text style={styles.title}>
+                Duen
+            </Text>
+            <Text style={styles.title}>
+                des
+            </Text>
+            
+        </View>
+    )
+}
---
+++ src/app/index/styles.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native"
+
+import { colors } from "../../styles/colors"
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    title: {
+        color: colors.vinho[100],
+        fontSize: 80,
+    },
+    titlea: {
+        color: colors.vinho[100],
+        fontSize: 11,
+    },
+})
